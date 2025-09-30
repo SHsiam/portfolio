d4638024d092175b008d4a0d43d26ab3e6ff2dbe
SHsiam <mdshiam4@gmail.com>
2025-09-30 02:05:22 +0100

    update

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,9 @@
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import { Link } from "react-router-dom";
-import { FaFacebook } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
-import { useRef } from 'react';
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { useRef } from "react";
 
 const Contact = () => {
   const form = useRef();
@@ -11,74 +11,103 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_fknrk97', 'template_mj39fou', form.current, 'aEO30HzZRxPNyMgEe')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_fknrk97",
+        "template_mj39fou",
+        form.current,
+        "aEO30HzZRxPNyMgEe"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-          if(result.text){
-            alert('Thank You');
-        }
-      }, (error) => {
+          if (result.text) {
+            alert("Thank You");
+          }
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
-    return (
-        <div>
-            <div className="hero min-h-screen  bg-slate-950 text-white">
-  <div className="hero-content flex-col lg:flex-row-reverse ">
-    <div className="text-center lg:text-left px-6 ">
-      <h1 className="text-3xl font-bold">Contact</h1>
-      <p className="py-2">mdshiam4@gmail.com</p>
-      <p className="py-2">+8801817791056</p>
-      <div>
-  <div className="flex gap-2">
-    <div >
-    <Link to='https://web.facebook.com/sh.shiam.5/' >
-   <FaFacebook className="w-6 h-8"/>
-   </Link>
-    </div>
-  
-   <Link to='https://www.linkedin.com/in/sayed-hossain-bhuiyan/'>
-   <FaLinkedin className="w-6 h-8" />
-   </Link>
-   <Link to='https://github.com/SHsiam'>
-   <FaGithub className="w-6 h-8"/>
-   </Link>
-  </div>
-</div>
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-slate-900 text-black">
-      <div className="card-body font-bold ">
-        <form ref={form} onSubmit={sendEmail}>
-      <div className="form-control">
-          <label className="label ">
-            <span className="label-text text-white text-xl ">Name</span>
-          </label>
-          <input type="text" name="user_name" placeholder="Name" className="input input-bordered" />
-        </div>
-        <div className="form-control ">
-          <label className="label ">
-            <span className="label-text text-white text-xl  ">Email</span>
-          </label>
-          <input type="email" placeholder="Email" name="user_email" className="input input-bordered" />
-        </div>
+  return (
+    <div>
+      <div className="hero min-h-screen  bg-slate-950 text-white">
+        <div className="hero-content flex-col lg:flex-row-reverse ">
+          <div className="text-center lg:text-left px-6 ">
+            <h1 className="text-3xl font-bold">Contact</h1>
+            <p className="py-2">mdshiam4@gmail.com</p>
+            <p className="py-2">+8801817791056</p>
+            <div>
+              <div className="flex gap-2">
+                <div>
+                  <Link to="https://www.facebook.com/shsiam03/">
+                    <FaFacebook className="w-6 h-8" />
+                  </Link>
+                </div>
 
-        <div className="form-control ">
-          <label className="label ">
-            <span className="label-text text-white text-xl">Message</span>
-          </label>
-          <textarea className="textarea textarea-bordered" placeholder="Message"></textarea>
+                <Link to="https://www.linkedin.com/in/sayed-hossain-bhuiyan/">
+                  <FaLinkedin className="w-6 h-8" />
+                </Link>
+                <Link to="https://github.com/SHsiam">
+                  <FaGithub className="w-6 h-8" />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-slate-900 text-black">
+            <div className="card-body font-bold ">
+              <form ref={form} onSubmit={sendEmail}>
+                <div className="form-control">
+                  <label className="label ">
+                    <span className="label-text text-white text-xl ">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="user_name"
+                    placeholder="Name"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control ">
+                  <label className="label ">
+                    <span className="label-text text-white text-xl  ">
+                      Email
+                    </span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    name="user_email"
+                    className="input input-bordered"
+                  />
+                </div>
+
+                <div className="form-control ">
+                  <label className="label ">
+                    <span className="label-text text-white text-xl">
+                      Message
+                    </span>
+                  </label>
+                  <textarea
+                    className="textarea textarea-bordered"
+                    placeholder="Message"
+                  ></textarea>
+                </div>
+                <div className="form-control mt-6">
+                  <input
+                    className="text-white font-bold btn bg-slate-800 border-white hover:bg-slate-400 rounded"
+                    type="submit"
+                    value="Submit"
+                  ></input>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="form-control mt-6">
-          <input className="text-white font-bold btn bg-slate-800 border-white hover:bg-slate-400 rounded" type="submit" value='Submit'></input>
-        </div>
-        </form>
       </div>
-
     </div>
-  </div>
-</div>
-        </div>
-    );
+  );
 };
 
 export default Contact;
